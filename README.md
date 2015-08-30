58fd0d15c3fb88e88cbbf2dd7639e3744b739a6d
austincarvey <austin@carvey.me>
2015-08-30 08:52:07 -0400

    Pushing to gh

---
+++ README.md
@@ -0,0 +1,15 @@
+# SpaceAppsNextGen
+
+2015 August SpaceApps Hackathon Project
+
+by
+
+William Zheng
+
+Sophia Pao
+
+Ajay Dheeraj
+
+Austin Carvey
+
+Disorn Kwunchaithanya
---
+++ map.js
@@ -0,0 +1,12 @@
+var map;
+
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(7.317238425217869, 8),
+    zoom:2,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+initialize();
