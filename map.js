fcd8e736981df479f8fa0fb84d34c3271cc6e145
Ajay Dheeraj <and38@duke.edu>
2017-09-26 16:36:13 -0400

    ayy

--- map.js
+++ map.js
@@ -9,4 +9,7 @@ function initialize() {
   map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
-initialize();
+$(document).ready(function(){
+  initialize();
+});
+//initialize();
